61ebfd30863c4972f3ab83d140fd1dad031aaee6
giacomo cordella <giacomo@codemedia.it>
2023-02-17 01:58:51 +0100

    start with 2 main q's

--- src/components/Riassuntivo.vue
+++ src/components/Riassuntivo.vue
@@ -9,17 +9,19 @@ import HistoryQueueBox from './HistoryQueueBox.vue'
     data() {
       return {
         prefix: 'A',
-        q: 1,
+        prefix2: 'B',
+        q1: 1,
+        q2: 1,
         history: []
       }
     },
     watch: {
-      q: function (newQ, oldQ) {    
+      q1: function (newQ, oldQ) {    
           if(this.history.length < 50) {
             this.pushToHistory(oldQ); 
             this.history = [...new Set(this.history)]
             for( let i = 0; i < this.history.length; i++ ) {
-              if( this.history[i] === this.q) {
+              if( this.history[i] === this.q1) {
                 this.history.splice(i, 1)
               }
             }
@@ -27,12 +29,12 @@ import HistoryQueueBox from './HistoryQueueBox.vue'
       }
     },
     methods: {
-      increment() {
-        this.q++
+      increment(queue) {
+        this.queue++
       },
-      decrement() {
-        if(this.q >= 1) {
-          this.q--
+      decrement(queue) {
+        if(this.queue >= 1) {
+          this.queue--
         }
       },
       pushToHistory(q) {
@@ -45,9 +47,19 @@ import HistoryQueueBox from './HistoryQueueBox.vue'
 
 <template>
 
-  <QueueBox :q="q" :prefix="prefix"></QueueBox>
-  <button class="btn" @click="decrement">Prev</button>
-  <button class="btn" @click="increment">Next</button>
+  <div style="display: flex">
+    <div>
+      <QueueBox :q="q1" :prefix="prefix" :key="1"></QueueBox>
+      <button class="btn" @click="decrement(q1)">Prev</button>
+      <button class="btn" @click="increment(q1)">Next</button>
+    </div>
+    <div>
+      <QueueBox :q="q2" :prefix="prefix2" :key="2"></QueueBox>
+      <button class="btn" @click="decrement(q2)">Prev</button>
+      <button class="btn" @click="increment(q2)">Next</button>
+    </div>
+  </div>
+
 
   <HistoryQueueBox v-for="i in 5" :key="Math.random()" :q="this.history[i-1]"></HistoryQueueBox>
 </template>
