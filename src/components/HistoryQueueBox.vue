e8714c58efa702eac8b71495173788b295f6e963
giacomo cordella <giacomo@codemedia.it>
2023-02-17 01:24:07 +0100

    first commit

---
+++ src/components/HistoryQueueBox.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        props: {
+            q: Number
+        }
+    }
+</script>
+
+<template>
+    <div class="historyQueueBox">
+        <div class="queue historyQueue">
+            <h1 v-if="q">{{ (q).toString().padStart(2,0) }}</h1>
+        </div>
+    </div>
+</template>
